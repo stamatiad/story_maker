c9d314c30a015b17a3e2beb511cacfb5ea3b82ce
stevest <stamatiad.st@gmail.com>
2019-03-16 00:09:16 +0200

    Updates

---
+++ precache-manifest.923face8751516ea6866dfcb041ea2e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/story_maker/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ab94b83f3ff31f699e9c",
+    "url": "/story_maker/static/js/runtime~main.2c0617dc.js"
+  },
+  {
+    "revision": "ed933453e514be417612",
+    "url": "/story_maker/static/js/main.e475af66.chunk.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "/story_maker/static/js/2.aa7a0768.chunk.js"
+  },
+  {
+    "revision": "ed933453e514be417612",
+    "url": "/story_maker/static/css/main.10f33190.chunk.css"
+  },
+  {
+    "revision": "ab323004892c7b15c472dfafa48c8261",
+    "url": "/story_maker/index.html"
+  }
+];
